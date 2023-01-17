512a7e4112a71c63f35d9b338dc77c609cbf4596
Richard Wohlbold <richard@rgwohlbold.de>
2023-01-17 21:46:03 +0100

    Remove more unneeded files

--- config.go
+++ config.go
@@ -10,14 +10,6 @@ type Config struct {
 	GitLabApiToken    string `yaml:"gitlab_api_token"`
 }
 
-type ConfigSignature struct {
-	Name     string `yaml:"name"`
-	Part     string `yaml:"part"`
-	Match    string `yaml:"match,omitempty"`
-	Regex    string `yaml:"regex,omitempty"`
-	Verifier string `yaml:"verifier,omitempty"`
-}
-
 func ParseConfig() (*Config, error) {
 	config := &Config{}
 	var (
